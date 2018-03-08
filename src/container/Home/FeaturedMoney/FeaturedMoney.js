3b239686c57ed291c80925d577014d147b652ef5
DiamondCrack <zk05161219@gmail.com>
2018-03-08 14:56:25 +0800

    add component FeaturedMoney

---
+++ src/container/Home/FeaturedMoney/FeaturedMoney.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+import Panel from '../../../core/Panel/Panel'
+
+import './FeaturedMoney.css'
+
+export default class FeaturedMoney extends Component {
+    state={
+        data:[
+            {
+            title:'定期理财',
+            sub:'90天可质押',
+            rate:'5.5%',
+            text:'历史年化收益率'
+        },{
+            title:'固收理财',
+            sub:'理财推荐',
+            rate:'5.8%',
+            text:'综合年化收益率'
+        },{
+            title:'基智账户',
+            sub:'组合投资',
+            rate:'8%-10%',
+            text:'止盈年化收益'
+        },
+        {
+            title:'小白基金',
+            sub:'超短期',
+            rate:'5%',
+            text:'7日年化收益率'
+        }
+        ]
+    }
+    render() {
+        return (
+            <div className='featured-money'>
+                <Panel title='理财精选' >
+                    <section className="content">
+                        {this.state.data.map(item=>(
+                            <dl key={item.title} className='item'>
+                                <dt>{item.title}<span>{item.sub}</span></dt>
+                                <dd>{item.rate}</dd>
+                                <dd>{item.text}</dd>
+                            </dl>
+                        ))}
+                    </section>
+                </Panel>
+            </div>
+        )
+    }
+}
--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import AutoSlider from './AutoSlider/AutoSlider'
-import NewHand from './NewHand/NewHand'
-import QuickBorrow from './QuickBorrow/QuickBorrow'
+import FeaturedMoney from './FeaturedMoney/FeaturedMoney'
+// import NewHand from './NewHand/NewHand'
+// import QuickBorrow from './QuickBorrow/QuickBorrow'
 
 export default class componentName extends Component {
     render() {
         return (
         <div>
             <AutoSlider/>
-            <NewHand/>
-            <QuickBorrow/>
+            {/* <NewHand/>
+            <QuickBorrow/> */}
+            <FeaturedMoney/>
         </div>
         )
     }
