7ac80b5ff179b424d88ecfd46e10dc35921c55ab
DiamondCrack <zk05161219@gmail.com>
2018-03-09 14:32:30 +0800

    update Component NoteBonus

--- src/container/WhiteNote/NoteBonus/NoteBonus.js
+++ src/container/WhiteNote/NoteBonus/NoteBonus.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react'
 import Panel from '../../../core/Panel/Panel'
 
+import './NoteBonus.css'
 export default class NoteBonus extends Component {
+    state={
+        data:[
+            {
+                title:'开通白条',
+                sub:'送百元礼包',
+                img:'//img12.360buyimg.com/jrpmobile/jfs/t4378/62/155957680/9017/16463551/58affd58Nb5c9198e.jpg?width=120&height=120',
+                linkText:'立即查看>',
+            },
+            {
+                title:'白条提额',
+                sub:'金融app专享提额礼包',
+                img:'//img12.360buyimg.com/jrpmobile/jfs/t4270/96/1102370053/8651/58b7fa5a/58bd2e10N453e9464.jpg?width=120&height=120',
+                linkText:'去提额>',
+            }
+        ]
+    }
     render() {
         return (
             <div className='note-bonus'>
                 <Panel title='白条福利'>
-                    <ul>
-                        <li>a</li>
-                    </ul>
+                    {this.state.data.map(item=>(
+                        <div className='item'>
+                            <img src={item.img} alt={item.title}/>
+                            <div>
+                                <h4>{item.title}</h4>
+                                <p>{item.sub}</p>
+                            </div>
+                            <em>
+                                {item.linkText}
+                            </em>
+                        </div>
+                    ))}
                 </Panel>
             </div>
         )
     }
-    }
+}
--- src/container/WhiteNote/WhiteNote.js
+++ src/container/WhiteNote/WhiteNote.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import AdFirst from '../../container/WhiteNote/AdFirst/AdFirst'
+import NoteBonus from './NoteBonus/NoteBonus'
 export default class WhiteNote extends Component {
     render() {
         return (
             <div className='white-note'>
                 <AdFirst/>
+                <NoteBonus/>
             </div>
         )
     }
