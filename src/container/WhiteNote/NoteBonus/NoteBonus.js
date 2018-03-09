04c154f13c49825c85e096e9f9b5e79c1b15ec7a
DiamondCrack <zk05161219@gmail.com>
2018-03-09 14:05:28 +0800

    add Component NoteBonus

---
+++ src/container/WhiteNote/NoteBonus/NoteBonus.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Panel from '../../../core/Panel/Panel'
+
+export default class NoteBonus extends Component {
+    render() {
+        return (
+            <div className='note-bonus'>
+                <Panel title='白条福利'>
+                    <ul>
+                        <li>a</li>
+                    </ul>
+                </Panel>
+            </div>
+        )
+    }
+    }
