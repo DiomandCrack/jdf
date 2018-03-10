62ba451f3bf3300263a937b42a747f4a0c25d56b
DiamondCrack <zk05161219@gmail.com>
2018-03-10 15:41:51 +0800

    add page Download

--- src/container/Special/Download/Download.js
+++ src/container/Special/Download/Download.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import Panel from '../../../core/Panel/Panel'
+import './Download.css'
 export default class Download extends Component {
     render() {
         return (
             <div className='download'>
                 <Panel>
                     <section className='content'>
-                        a
+                        <div className="item">
+                        </div>
+                        <div className='btn-con'>
+                            <p>立即下载</p>
+                        </div>
+                        <p className="text">
+                            copyright
+                        </p>
                     </section>
                 </Panel>
             </div>
